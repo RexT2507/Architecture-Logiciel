1cce993c4c98d3ba241360bb9d0e1278331137b2
RexT2507 <baptiste.marmier@ynov.com>
2020-06-04 16:48:56 +0200

    [DEV] Mise en place de GetProducts V1 et V2

---
+++ src/Services/Catalog/Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ApiVersion("1", Deprecated = true)]
+        public async Task<ActionResult> GetProducts()
+        {
+            return Ok(new List<Product>());
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ApiVersion("2")]
+        public async Task<ActionResult> GetProducts_V2()
+        {
+            return Ok(new List<Product>());
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -16,6 +16,8 @@ namespace Catalog.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiVersioning();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +28,19 @@ namespace Catalog.API
                 app.UseDeveloperExceptionPage();
             }
 
+            /*app.Use(test);*/
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
+
+        /*private RequestDelegate test(RequestDelegate s)
+        {
+            return s;
+        }*/
     }
 }
