a5ca6a95be5f7d4ab3ba066f343316c92e4dab1b
RexT2507 <baptiste.marmier@ynov.com>
2020-06-05 10:00:27 +0200

    [DEV] Ajout du CatalogDbContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasIndex("Name");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
